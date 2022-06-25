6a36044d2535269467b1d34a769b80ff5a7c4101
root <fazleh2010@gmail.com>
2022-06-25 13:31:43 +0200

    execution time added

--- Fintan/apertium-rdf-to-tsv/apertium-run.sh
+++ Fintan/apertium-rdf-to-tsv/apertium-run.sh
@@ -4,6 +4,10 @@ HERE=`echo $0 | sed -e s/'^[^\/]*$'/'.'/g -e s/'\/[^\/]*$'//`;
 ROOT=$HERE/../../..;
 
 
+START=$(date +%s)
+
 sudo sh ./run.sh -c apertium-pipeline.json -p https://github.com/apertium/apertium-trunk.git en es >>apertium-RDF-to-TSV-output.tsv
 
-
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+echo "ApertiumRDF to ApertiumTSV: $DIFF seconds"
--- Fintan/conll-to-rdf/conll-to-RDF-run.sh
+++ Fintan/conll-to-rdf/conll-to-RDF-run.sh
@@ -3,13 +3,21 @@
 HERE=`echo $0 | sed -e s/'^[^\/]*$'/'.'/g -e s/'\/[^\/]*$'//`;
 ROOT=$HERE/../../..;
 
-set startTime=%time%
+START=$(date +%s)
 
 sudo sh ./run.sh -c conll-small-pipeline.json >> conll-rdf-small-output-actual.ttl
 
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+echo "conll to rdf(small): $DIFF seconds"
+
+START=$(date +%s)
+
 sudo sh ./run.sh -c conll-large-pipeline.json >> conll-rdf-large-output-actual.ttl
 
-echo Start Time: %startTime%
-echo Finish Time: %time%
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+echo "conll to rdf(large): $DIFF seconds"
+
 
 
